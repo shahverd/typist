4508c22fdd80529a07f3f5541f37c927d4d69b29
Mostafa Shahverdy <shahverdy.m@gmail.com>
2021-09-20 06:50:26 +0430

    Refactoring file structure

---
+++ index_script.js
@@ -0,0 +1,67 @@
+
+function setCookie(name, value, daysToLive) { var cookie = name + "=" + encodeURIComponent(value); if(typeof daysToLive === "number") { cookie += "; max-age=" + (daysToLive*24*60*60); document.cookie = cookie; } }
+function getCookie(name) { var cookieArr = document.cookie.split(";"); for(var i = 0; i < cookieArr.length; i++) { var cookiePair = cookieArr[i].split("="); if(name == cookiePair[0].trim()) { return decodeURIComponent(cookiePair[1]); } } return 0; }
+// Tabbed Menu
+function openMenu(evt, menuName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+    }
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+}
+document.getElementById("myLink").click();
+
+
+
+
+fetch('https://gist.githubusercontent.com/shahverd/841ebfebd28286e614bfc2fc057869e1/raw')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach((course, index) => {
+
+            title = document.createElement("b"); 
+
+            title_link = document.createElement("a");
+            title_link.href = "pracitce.html#" + course.hash;
+            title_link.target = "_blank";
+            title_link.innerHTML = (index+1) + "- " + course.title;     
+
+            title.appendChild(title_link);
+
+            speed = document.createElement("span"); 
+            if(getCookie(course.hash) != null){
+                speed.innerHTML = " بهترین:" + getCookie(course.hash + "_max") +
+                                  "؛ میانگین: " + getCookie(course.hash + "_avg") + 
+                                  " دفعات: " + getCookie(course.hash + "_count") + " بار";
+                reset = document.createElement("a");
+                reset.innerHTML = "<u>بازنشانی آمار</u>";
+                reset.style.cursor = "pointer";
+                reset.style.float = "left";
+                reset.onclick = function(e){
+                    setCookie(course.hash + "_max", 0, 100000);
+                    setCookie(course.hash + "_avg", 0, 100000);
+                    setCookie(course.hash + "_count", 0, 100000);
+
+                    location.reload();
+                }
+                speed.append(reset);
+            }
+            speed.className = "w3-text-grey";
+
+            br = document.createElement("br"); 
+
+            document.getElementById("courses").appendChild(title);      
+            document.getElementById("courses").appendChild(speed);      
+            document.getElementById("courses").appendChild(br);      
+
+        });
+    });
+
+
+

5cb0a83f56d76fc15c3e889ba70b6915002e1259
Mostafa Shahverdy <shahverdy.m@gmail.com>
2024-02-23 23:49:04 +0330

    Temp Refactor

--- assets/js/practice.js
+++ assets/js/practice.js
@@ -1,14 +1,28 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-to_type = $("#to_type");
-typed = $("#typed");
-res_div = $("#res_div");
-keyboard = $("#keyboard_practice");
-result = $("#result");
-mistakes = $("#mistakes");
-text_length = $("#text_length");
-repeat = $("#repeat");
+const DOM_to_type       = $("#to_type");
+const DOM_typed         = $("#typed");
+const DOM_res_div       = $("#res_div");
+const DOM_keyboard      = $("#keyboard_practice");
+const DOM_result        = $("#result");
+const DOM_mistakes      = $("#mistakes");
+const DOM_text_length   = $("#text_length");
+const DOM_repeat        = $("#repeat");
+const DOM_result_best   = $("#best")
+
+
+const COLOR_TYPED_MISTAKE = "red"
+const COLOR_TYPED_CORRECT = "gery"
+const COLOR_TYPED_ACCEPTED = "blue"
+
+const COLORED_KEY_FORGROUND = "white"
+const COLORED_KEY_MOST_MISTAKES = "red"
+
+const CHAR_SPACE = '␣'
+
+const CHAR_HISTORY_LENGHTH = 20;
+const NUMBER_OF_WORDS_PER_PRACTICE = 30
 
 //prevent defualt behavior of space key which is scrolling the page
 window.addEventListener('keypress', function (e) {
@@ -30,50 +44,17 @@ $("#iframe_overlay").onclick = function (e) {
     e.preventDefault();
 }
 
-dataset = window.location.search.replace("?", "");
+const WORDS_COLLECTION = window.location.search.replace("?", "");
 
-var data_path = '../data/ai';
-var num_chars = 0;
-var num_words = 0;
-var t0 = null;
-var mistake_count = 0;
-var repeated_mistake_flag = false;
+let data_path = '../data/ai';
+let num_chars = 0;
+let num_words = 0;
+let t0 = null;
+let mistake_count = 0;
+let repeated_mistake_flag = false;
 
-document.onkeypress = process_keypress;
-
-new_run();
 
 /////////////////////////////////////////////////////////////////////////////////////
-function new_run() {
-    mistake_count = 0;
-    t0 = null;
-    repeated_mistake_flag = false;
-
-    show_latest_results();
-
-    data = getValue("full_text");
-
-    if (!data) {
-        fetch(data_path)
-            .then(response => response.text())
-            .then(data => {
-                saveValue("full_text", data)
-                process_data(data, dataset);
-            });
-    } else {
-        process_data(data, dataset);
-    }
-}
-
-function show_latest_results() {
-    $("#result").innerHTML = getValue(dataset + "_last_result") || "0";
-
-    $("#mistakes").innerHTML =
-        (getValue(dataset + "_last_mistake_count") || "0") + " بار، "
-        + (getValue(dataset + "_last_mistake_precentage") || "0") + " درصد";
-
-    $("#best").innerHTML = getValue(dataset + "_max") || "0";
-}
 
 function saveValue(name, value) {
     window.localStorage.setItem(name, value);
@@ -94,237 +75,281 @@ function beep(st) {
     new Audio(url[st]).play();
 }
 
-function get_wrong_precentage(chr) {
-    var precentage = -1;
-
-    wrong_count = getValue("key_" + chr).split("0").length - 1;
-    total_length = getValue("key_" + chr).length;
-
-    if (total_length != 0)
-        precentage = wrong_count / total_length;
-
-    return precentage;
-}
-
-function load_keyboard_color() {
-
-    max_wrong = get_max_wrong();
 
 
-    keys = $$("#keyboard_practice .KeyboardKey text").forEach((v, i) => {
-
-        precentage = get_wrong_precentage(v.textContent);
-
-        color = "";
-
-        middle_pivot = 0.1; //less than this should go to green
-        top_pivot = 0.25 // more than this should be fully red
-        max_color_range = 126; // from 0 to 256
-
-        green_power = max_color_range / middle_pivot;
-        red_power = max_color_range / (top_pivot - middle_pivot);
-
-        if (0 <= precentage && precentage < middle_pivot) {
-            color = "rgb( " + (precentage * green_power) + ", " + max_color_range + ", 0)";
-            v.style.fill = "white";
-        }
-        if (middle_pivot <= precentage) {
-            color = "rgb( " + max_color_range + ", " + (max_color_range - (precentage - middle_pivot) * red_power) + ", 0)";
-            v.style.fill = "white";
-        }
-
-        v.parentNode.querySelector("rect").style.fill = color;
-
-        if (max_wrong <= precentage) {
-            v.parentNode.querySelector("rect").style.fill = "red";
-            v.style.fill = "white";
-
-            if (v.classList.contains("KeyboardKey-symbol--secondary"))
-                v.style.fill = "red";
-        }
-
-    });
-
-}
-
-function get_max_wrong() {
-
-    var key_history = [];
-    for (var i = 0; i < localStorage.length; i++) {
-
-        letters = [
-            "key_ض", "key_ص", "key_ث", "key_ق", "key_ف",
-            "key_غ", "key_ع", "key_ه", "key_خ", "key_ح",
-            "key_ج", "key_چ", "key_ش", "key_س", "key_ی",
-            "key_ب", "key_ل", "key_ا", "key_ت", "key_ن",
-            "key_م", "key_ک", "key_گ", "key_ظ", "key_ط",
-            "key_ز", "key_ر", "key_ذ", "key_د", "key_پ",
-            "key_و", "key_ئ", "key_ژ"
-        ];
 
 
-        if (localStorage.key(i).match(/key_/g) && letters.includes(localStorage.key(i))) {
-            key_history.push(localStorage.getItem(localStorage.key(i)))
-        }
+class Practice {
+    constructor() {
+
+
+        this.ــinit();
+
+    }
+    ــinit() {
+        this.__mistake_flag = false
+        this.__mistake_count = 0;
+
+        let chosen_words = this.__choose_words(word_list, WORDS_COLLECTION);  //from ai.js file
+        DOM_typed.innerHTML = null;
+        DOM_to_type.innerHTML = chosen_words;
+        DOM_to_type.firstChild.className = "blnk"; // blink on startup
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+
+        this.__update_keyboard_color()
     }
 
-    key_history = key_history.map((item) => {
-        wrong_count = item.split("0").length - 1;
-        total_length = item.length;
+    __update_keyboard_color() {
 
-        var precentage = -1;
+        let max_wrong = this.__get_max_percentage_of_wrong_char();
+
+        $$("#keyboard_practice .KeyboardKey text").forEach((v, i) => {
+
+            let precentage = this.__get_char_mistake_percentage(v.textContent);
+
+            let background_color = "";
+            let forground_color = "";
+
+            let MIDDLE_PIVOT = 0.1; //less than this should go to green
+            let TOP_PIVOT = 0.25 // more than this should be fully red
+            let MAX_COLOR_RANGE = 126; // from 0 to 256
+
+            let GREEN_POWER = MAX_COLOR_RANGE / MIDDLE_PIVOT;
+            let RED_POWER = MAX_COLOR_RANGE / (TOP_PIVOT - MIDDLE_PIVOT);
+
+            if (0 <= precentage && precentage < MIDDLE_PIVOT) {
+                background_color = "rgb( " + (precentage * GREEN_POWER) + ", " + MAX_COLOR_RANGE + ", 0)";
+                forground_color = COLORED_KEY_FORGROUND;
+            }
+            if (MIDDLE_PIVOT <= precentage) {
+                background_color = "rgb( " + MAX_COLOR_RANGE + ", " + (MAX_COLOR_RANGE - (precentage - MIDDLE_PIVOT) * RED_POWER) + ", 0)";
+                forground_color = COLORED_KEY_FORGROUND;
+            }
+
+            if (max_wrong <= precentage) {
+                background_color = COLORED_KEY_MOST_MISTAKES;
+                forground_color = COLORED_KEY_FORGROUND
+
+                if (v.classList.contains("KeyboardKey-symbol--secondary"))
+                    forground_color = COLORED_KEY_FORGROUND
+            }
+            
+            v.parentNode.querySelector("rect").style.fill = background_color;
+            v.style.fill = forground_color
+
+        });
+
+    }
+
+    __get_max_percentage_of_wrong_char() {
+
+        let key_history = [];
+        for (var i = 0; i < localStorage.length; i++) {
+
+            let letters = [
+                "key_ض", "key_ص", "key_ث", "key_ق", "key_ف",
+                "key_غ", "key_ع", "key_ه", "key_خ", "key_ح",
+                "key_ج", "key_چ", "key_ش", "key_س", "key_ی",
+                "key_ب", "key_ل", "key_ا", "key_ت", "key_ن",
+                "key_م", "key_ک", "key_گ", "key_ظ", "key_ط",
+                "key_ز", "key_ر", "key_ذ", "key_د", "key_پ",
+                "key_و", "key_ئ", "key_ژ"
+            ];
+
+            if (localStorage.key(i).match(/key_/g) && letters.includes(localStorage.key(i))) {
+                key_history.push(localStorage.getItem(localStorage.key(i)))
+            }
+        }
+
+        key_history = key_history.map((item) => {
+            let wrong_count = item.split("0").length - 1;
+            let total_length = item.length;
+
+            let precentage = -1;
+
+            if (total_length != 0)
+                precentage = wrong_count / total_length;
+
+            return precentage;
+
+        });
+
+        let max_wrong = key_history.sort().reverse()[0];
+        return max_wrong;
+    }
+
+    __get_char_mistake_percentage(chr) {
+        let precentage = -1;
+
+        let wrong_count = getValue("key_" + chr).split("0").length - 1; // number of 1 
+        let total_length = getValue("key_" + chr).length;
 
         if (total_length != 0)
             precentage = wrong_count / total_length;
 
         return precentage;
+    }
+    
 
-    });
 
+    __choose_words(words, WORDS_COLLECTION) {
 
-    max_wrong = key_history.sort().reverse()[0];
-    return max_wrong;
-}
+        words = words.split("|");
 
-function process_data(data, dataset) {
+        if (WORDS_COLLECTION != "ai") {
+            words = words.slice((WORDS_COLLECTION - 1) * words.length / 20, (WORDS_COLLECTION) * words.length / 20); // 1 20th of data
+        }
 
-    data = data.split("|");
+        let max_wrong = this.__get_max_percentage_of_wrong_char();
 
-    if (dataset != "ai") {
-        data = data.slice((dataset - 1) * data.length / 20, (dataset) * data.length / 20); // 1 20th of data
+        words = words.map(item => {
+            let sum = 0;
+
+            for (let i = 0; i < item.length; i++) {
+                if (this.__get_char_mistake_percentage(item[i]) == max_wrong) {
+                    sum += this.__get_char_mistake_percentage(item[i]);
+                }
+            }
+
+            return {
+                "word": item,
+                "grade": sum
+            }
+        });
+
+        words = words.sort((a, b) => {
+            return (Math.random() - 0.5) + 1 / (1 + a.grade) - 1 / (1 + b.grade);
+        })
+
+        words = words.map((item) => { return item.word })
+        words = words.slice(0, NUMBER_OF_WORDS_PER_PRACTICE);
+        words = words.join(" ");
+        
+        num_words = parseFloat(words.length) / 5; // as a standard way, words are considered to be 5 chars each
+        num_chars = words.length;
+
+        words = words.split("");
+        words = words.map(item => {
+            if (item == ' ')
+                item = '␣&#8203;';
+            return '<span>' + item + '</span>';
+        });
+        
+        return words.join("");
+
     }
 
-    max_wrong = get_max_wrong();
+    __move_cursor(){
+        DOM_to_type.firstChild.className = "";
+        DOM_typed.appendChild(DOM_to_type.firstChild);
 
-    data = data.map(item => {
-        sum = 0;
+        if (DOM_typed.lastChild.style.color == COLOR_TYPED_MISTAKE)
+            DOM_typed.lastChild.style.color = COLOR_TYPED_ACCEPTED;
+    
+        if (DOM_to_type.firstChild != null)
+            DOM_to_type.firstChild.className = "blnk";
 
-        for (i = 0; i < item.length; i++) {
-            if (get_wrong_precentage(item[i]) == max_wrong) {
-                sum += get_wrong_precentage(item[i]);
-            }
+        if(DOM_to_type.innerText.length == 0){ 
+            beep('finished');
+            this.__update_statistics_line(performance.now());
+            this.__update_keyboard_color();
+            this.__init_totype_line()
+            return
         }
+    };
 
-        return {
-            "word": item,
-            "grade": sum
-        }
-    });
+    __insert_key_history(char, value){
+        
+        let key = "key_" + char
+        let char_history = getValue(key);
 
-    data = data.sort((a, b) => {
-        return (Math.random() - 0.5) + 1 / (1 + a.grade) - 1 / (1 + b.grade);
-    })
-
-    //data.reverse();
-
-
-    data = data.map((item) => { return item.word })
-
-    data = data.slice(0, 25);
-
-    //data = data.sort(() => Math.random() - 0.5)
-
-    data = data.join(" ");
-
-    num_words = parseFloat(data.length) / 5;
-    num_chars = data.length;
-
-    data = data.split("");
-    data = data.map(item => {
-        if (item == ' ')
-            return '<span style="color:#222">␣&#8203;</span>';
-        return '<span>' + item + '</span>';
-    });
-    data = data.join("");
-
-    typed.innerHTML = null;
-    to_type.innerHTML = data;
-    to_type.firstChild.className = "blnk"; // blink on startup
-
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-
-    load_keyboard_color();
-}
-
-
-function process_keypress(evt) {
-
-    if (t0 == null)
-        t0 = performance.now();
-
-    evt = evt || window.event;
-
-
-    if ((evt.key == to_type.innerText[0]) || (evt.key == ' ' && to_type.innerText[0] == '␣')) {
-
-        beep("correct");
-
-        repeated_mistake_flag = false;
-
-        char_history = getValue("key_" + to_type.innerText[0]);
-        saveValue(
-            "key_" + to_type.innerText[0],
-            char_history.length > 20 ? char_history.substring(1) : char_history + "1"
+        saveValue( key,
+           char_history.length > CHAR_HISTORY_LENGHTH ? char_history.substring(1) : char_history + value
         );
 
-        to_type.firstChild.className = "";
-        typed.appendChild(to_type.firstChild);
+    };
 
-        if (typed.lastChild.style.color == "red")
-            typed.lastChild.style.color = "#3498DB";
+    __mark_first_char_of_totype_as_wrong(){
+        DOM_to_type.firstChild.style.color = COLOR_TYPED_MISTAKE;
+    }
 
-        if (to_type.innerText.length == 0) {
-            t1 = performance.now();
+    __update_statistics_line(finish_time) {
+        let words_per_minute = (num_words / (finish_time - this.start_time) * 60000);
 
-            wpm = (num_words / (t1 - t0) * 60000);
+        let final_res = Math.round(words_per_minute * 10) / 10;
 
-            final_res = Math.round(wpm * 10) / 10;
-
-            if (final_res > getValue(dataset + "_max")) {
-                saveValue(dataset + "_max", final_res);
-            }
-
-            saveValue(dataset + "_avg", Math.round((parseFloat(~~getValue(dataset + "_avg")) * parseFloat(~~getValue(dataset + "_count")) + final_res) / (~~parseFloat(getValue(dataset + "_count")) + 1) * 10) / 10);
-            saveValue(dataset + "_count", parseFloat(~~getValue(dataset + "_count")) + 1);
-
-            saveValue(dataset + "_last_result", final_res);
-            saveValue(dataset + "_last_mistake_count", mistake_count);
-            saveValue(dataset + "_last_mistake_precentage", Math.round(mistake_count / num_chars * 100));
-
-            result.innerHTML = final_res;
-            mistakes.innerHTML = mistake_count + " بار، " + Math.round(mistake_count / num_chars * 100) + " درصد";
-
-            //res_div.querySelector('p').style.opacity = "100%";
-
-            load_keyboard_color();
-
-            window.scroll({
-                top: 500,
-                behavior: 'smooth'
-            });
-
-            beep("finished");
-            new_run();
+        if (final_res > getValue(WORDS_COLLECTION + "_max")) {
+            saveValue(WORDS_COLLECTION + "_max", final_res);
         }
 
-    } else {
+        saveValue(WORDS_COLLECTION + "_avg", 
+              Math.round((parseFloat(~~getValue(WORDS_COLLECTION + "_avg")) 
+            * parseFloat(~~getValue(WORDS_COLLECTION + "_count")) + final_res) 
+              / (~~parseFloat(getValue(WORDS_COLLECTION + "_count")) + 1) * 10) / 10);
 
-        beep("wrong");
 
-        to_type.firstChild.style.color = "red";
+        saveValue(WORDS_COLLECTION + "_count", parseFloat(~~getValue(WORDS_COLLECTION + "_count")) + 1);
 
-        if (!repeated_mistake_flag) {
-            char_history = getValue("key_" + to_type.innerText[0]);
-            saveValue(
-                "key_" + to_type.innerText[0],
-                char_history.length > 20 ? char_history.substring(1) : char_history + "0"
-            );
-            mistake_count++;
-            repeated_mistake_flag = true;
+        saveValue(WORDS_COLLECTION + "_last_result", final_res);
+        saveValue(WORDS_COLLECTION + "_last_mistake_count", this.__mistake_count);
+        saveValue(WORDS_COLLECTION + "_last_mistake_precentage", Math.round(this.__mistake_count / num_chars * 100));
+
+        /////////////////////////////////////
+
+        DOM_result.innerHTML = getValue(WORDS_COLLECTION + "_last_result") || "0";
+
+        DOM_mistakes.innerHTML =
+              (getValue(WORDS_COLLECTION + "_last_mistake_count")      || "0") + " بار، "
+            + (getValue(WORDS_COLLECTION + "_last_mistake_precentage") || "0") + " درصد";
+
+        DOM_result_best.innerHTML = getValue(WORDS_COLLECTION + "_max") || "0";
+    }
+
+    __init_totype_line(){
+        this.ــinit();
+        this.start_time = performance.now()
+    }
+
+    __process_keypress(evt){
+        if(DOM_typed.innerText.length == 0){ //we havn't start typing yet.
+            this.start_time = null;
+        }
+
+        let current_expected_key;
+        try {
+            current_expected_key = DOM_to_type.innerText[0]
+        } catch (e) {
+            console.log("FINISHED WORDS>")
+        }
+
+        let current_pressed_key = evt.key;
+        
+        if(current_pressed_key === ' '){
+            current_pressed_key = CHAR_SPACE
+        }
+
+        if (current_expected_key === current_pressed_key){
+            this.__mistake_flag = false;
+            beep("correct")
+
+            this.__insert_key_history(current_expected_key, "1") // 1 means correct and 0 mean wrong
+            this.__move_cursor()
+
+        }else{
+            beep("wrong")
+            if(!this.__mistake_flag){
+                this.__mark_first_char_of_totype_as_wrong()
+                this.__insert_key_history(current_expected_key, "0") // 1 means correct and 0 mean wrong
+                this.__mistake_count ++;
+            }
+
+            this.__mistake_flag = true;
         }
     }
 
-    if (to_type.firstChild != null)
-        to_type.firstChild.className = "blnk";
 
-};
+}
+
+const practice = new Practice();
+
+document.onkeypress = (evt) => practice.__process_keypress(evt);
